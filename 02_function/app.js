135843bcb766941f0816d64682768dcf04b4d0a4
yiinuma <y.iinuma211@gmail.com>
2021-12-25 10:55:04 +0900

    02

---
+++ 02_function/app.js
@@ -0,0 +1,31 @@
+// 独自関数
+// function areaOfTriangle(base, height) {
+//   return (base * height) / 2;
+// }
+
+// const result = areaOfTriangle(10, 5);
+// console.log(result); //25
+
+// 無名関数
+const areaOfTriangle = function (base, height) {
+  return (base * height) / 2;
+};
+const result = areaOfTriangle(10, 5);
+console.log(result); //25
+
+// アロー関数
+const areaOfTriangle2 = (base, height) => {
+  return (base * height) / 2;
+};
+const result2 = areaOfTriangle2(100, 5);
+console.log(result2); //250
+
+// アロー関数・引数がない場合
+const sayHello = () => 'Hello';
+console.log(sayHello());
+
+// アロー関数・引数が１つの場合 vscodeの機能で()が付与されるが()なしでもOK
+const sayName = (name) => {
+  console.log('Hello ' + name);
+};
+sayName('Yoshi');
