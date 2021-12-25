b05875f636d41d4bb11717c5054c2a8208bedad9
yiinuma <y.iinuma211@gmail.com>
2021-12-25 11:44:42 +0900

    03

---
+++ 03_fourArithmeticOperations/app.js
@@ -0,0 +1,15 @@
+console.log(10 + 10); //20
+console.log(4 - 2); //2
+console.log(5 * 2); //10
+console.log(8 / 2.5); //3.2
+console.log(5 % 2); //1
+
+let num = 20;
+console.log(++num); //21
+console.log(num++); //21
+console.log(num); //22
+
+let num2 = 20;
+console.log(--num2); //19
+console.log(num2--); //19
+console.log(num2); //18
