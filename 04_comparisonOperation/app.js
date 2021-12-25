5d0aee38a38e6e745189a2fc9ee3b6b9886f793f
yiinuma <y.iinuma211@gmail.com>
2021-12-25 12:09:53 +0900

    04

---
+++ 04_comparisonOperation/app.js
@@ -0,0 +1,15 @@
+const result1 = 8 > 2;
+const result2 = 3 >= 3;
+const result3 = 7 < 4;
+const result4 = 3 <= 6;
+const result5 = '7' == 7;
+const result6 = '7' === 7;
+const result7 = '7' !== 7;
+
+console.log(result1); //true
+console.log(result2); //true
+console.log(result3); //false
+console.log(result4); //true
+console.log(result5); //true
+console.log(result6); //false
+console.log(result7); //true
