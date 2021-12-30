048db8bdbc639496638eb05e05117be237175766
yiinuma <y.iinuma211@gmail.com>
2021-12-30 14:33:42 +0900

    12

--- 12_class/app.js
+++ 12_class/app.js
@@ -33,5 +33,30 @@ class CPlayer {
   }
 }
 
-const CStartPlayer = new CPlayer('山田', 'セカント');
+const CStartPlayer = new CPlayer('山田', 'セカンド');
 CStartPlayer.CSayHello();
+
+// 継承 親クラスのメソッドを呼び出す
+// 親のクラスに定義されているメソッドを呼び出すことができる
+// class クラス名 extends 親のクラス名{
+//   //クラスに定義したいプロパティやメソッド
+//   constructor(引数) {
+//     super(引数)
+//   }
+//   メソッド名(引数) {
+//     super.親のメソッド名(引数)
+//   }
+// }
+class Women extends CPlayer {
+  constructor(name, position, number) {
+    super(name, position);
+    this.number = number;
+  }
+  // オーバーライト
+  CSayHello() {
+    console.log(`うちは、${this.position}の${this.name}で、背番号は${this.number}よ`);
+  }
+}
+
+const womenPlayer = new Women('レイナ', 'ファースト', 2);
+womenPlayer.CSayHello(); //うちは、ファーストのレイナで、背番号は2よ
